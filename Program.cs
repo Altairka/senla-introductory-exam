2e4040e74a69b6f3437b2b832067f69d7dd9ccad
NOONE FROM NOWHERE <altair999z@gmail.com>
2018-05-18 21:02:03 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace senla_introductory_exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
